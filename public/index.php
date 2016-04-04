6b66285b7f9884f8df6968f61e812a64a662f2fc
Alex Liu <lxiangcn@gmail.com>
2016-04-04 20:57:08 +0800

    update codeigniter to 3.0.6

--- public/index.php
+++ public/index.php
@@ -53,7 +53,7 @@
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
-
+//define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
 switch (strtolower(gethostname())) {
 case 'i-nxqx1cvw':
     define('ENVIRONMENT', 'production');
@@ -216,55 +216,87 @@ if (!is_dir($system_path)) {
  * Now that we know the path, set the main path constants
  * -------------------------------------------------------------------
  */
-// The name of THIS file
-define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
+    // The name of THIS file
+    define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
 
-// Path to the system folder
-define('BASEPATH', str_replace('\\', '/', $system_path));
+    // Path to the system directory
+    define('BASEPATH', $system_path);
 
-// Path to the front controller (this file)
-define('FCPATH', dirname(__FILE__) . '/');
+    // Path to the front controller (this file) directory
+    define('FCPATH', dirname(__FILE__).DIRECTORY_SEPARATOR);
 
-// Name of the "system folder"
-define('SYSDIR', trim(strrchr(trim(BASEPATH, '/'), '/'), '/'));
+    // Name of the "system" directory
+    define('SYSDIR', basename(BASEPATH));
 
-// The path to the "application" folder
-if (is_dir($application_folder)) {
-    if (($_temp = realpath($application_folder)) !== FALSE) {
-        $application_folder = $_temp;
+    // The path to the "application" directory
+    if (is_dir($application_folder))
+    {
+        if (($_temp = realpath($application_folder)) !== FALSE)
+        {
+            $application_folder = $_temp;
+        }
+        else
+        {
+            $application_folder = strtr(
+                rtrim($application_folder, '/\\'),
+                '/\\',
+                DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
+            );
+        }
     }
-
-    define('APPPATH', $application_folder . DIRECTORY_SEPARATOR);
-} else {
-    if (!is_dir(BASEPATH . $application_folder . DIRECTORY_SEPARATOR)) {
+    elseif (is_dir(BASEPATH.$application_folder.DIRECTORY_SEPARATOR))
+    {
+        $application_folder = BASEPATH.strtr(
+            trim($application_folder, '/\\'),
+            '/\\',
+            DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
+        );
+    }
+    else
+    {
         header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-        echo 'Your application folder path does not appear to be set correctly. Please open the following file and correct this: ' . SELF;
+        echo 'Your application folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF;
         exit(3); // EXIT_CONFIG
     }
 
-    define('APPPATH', BASEPATH . $application_folder . DIRECTORY_SEPARATOR);
-}
+    define('APPPATH', $application_folder.DIRECTORY_SEPARATOR);
 
-// The path to the "views" folder
-if (!is_dir($view_folder)) {
-    if (!empty($view_folder) && is_dir(APPPATH . $view_folder . DIRECTORY_SEPARATOR)) {
-        $view_folder = APPPATH . $view_folder;
-    } elseif (!is_dir(APPPATH . 'views' . DIRECTORY_SEPARATOR)) {
-        header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-        echo 'Your view folder path does not appear to be set correctly. Please open the following file and correct this: ' . SELF;
-        exit(3); // EXIT_CONFIG
-    } else {
-        $view_folder = APPPATH . 'views';
+    // The path to the "views" directory
+    if ( ! isset($view_folder[0]) && is_dir(APPPATH.'views'.DIRECTORY_SEPARATOR))
+    {
+        $view_folder = APPPATH.'views';
     }
-}
+    elseif (is_dir($view_folder))
+    {
+        if (($_temp = realpath($view_folder)) !== FALSE)
+        {
+            $view_folder = $_temp;
+        }
+        else
+        {
+            $view_folder = strtr(
+                rtrim($view_folder, '/\\'),
+                '/\\',
+                DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
+            );
+        }
+    }
+    elseif (is_dir(APPPATH.$view_folder.DIRECTORY_SEPARATOR))
+    {
+        $view_folder = APPPATH.strtr(
+            trim($view_folder, '/\\'),
+            '/\\',
+            DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
+        );
+    }
+    else
+    {
+        header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
+        echo 'Your view folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF;
+        exit(3); // EXIT_CONFIG
+    }
 
-if (($_temp = realpath($view_folder)) !== FALSE) {
-    $view_folder = $_temp . DIRECTORY_SEPARATOR;
-} else {
-    $view_folder = rtrim($view_folder, '/\\') . DIRECTORY_SEPARATOR;
-}
-
-define('VIEWPATH', $view_folder);
+    define('VIEWPATH', $view_folder.DIRECTORY_SEPARATOR);
 
 /*
  * --------------------------------------------------------------------------
